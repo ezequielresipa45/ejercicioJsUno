8f03afcec413b7d39c3a607387545966c55442cb
Ezequiel Resipa <87780295+ezequielresipa45@users.noreply.github.com>
2022-06-01 17:02:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,62 @@
-// alert(
-//   "Hola mi nombre es Luci, jugemos un juego!!! Me dirás 5 personas con distintas edades y adivinaré quien es el más grande, ¿Listo?"
-// );
-
-// let arregloDePersonas = [];
-
-// let arregloDeEdades = [];
-
-// for (let j = 1; j <= 5; j++) {
-//   arregloDePersonas.push(prompt(`Escriba el nombre de la persona numero ${j}`));
-//   arregloDeEdades.push(
-//     prompt(`Cual es la edad de ${arregloDePersonas[j - 1]}`)
-//   );
-// }
-
-// let personas = [
-//   [arregloDePersonas[0], arregloDeEdades[0]],
-//   [arregloDePersonas[1], arregloDeEdades[1]],
-//   [arregloDePersonas[2], arregloDeEdades[2]],
-//   [arregloDePersonas[3], arregloDeEdades[3]],
-//   [arregloDePersonas[4], arregloDeEdades[4]],
-// ];
-
-// almacenNumero = personas[0][1];
-
-// let nombre;
-
-// for (let index = 0; index < personas.length; index++) {
-//   if (personas[index][1] > almacenNumero) {
-//     almacenNumero = personas[index][1];
-
-//     nombre = personas[index][0];
-//   }
-// }
-
-// alert(`La persona con más edad es ${nombre} con ${almacenNumero} años.`);
-
-let consulta = Number(
-  prompt(
-    "Bienvenido a nuestro banco, ¿Que desea hacer? Presione 1 para DEPÓSITO - Presione 2 para EXTRACCIÓN"
-  )
-);
-
 let billetera = 0;
 
-let depositar = (valorDepositar) => {
-  billetera += valorDepositar;
+let value = true;
 
-  return alert(
-    `Usted ah depositado $${valorDepositar}, su saldo ahora es de $${billetera}. `
+while (value === true) {
+  let consulta = Number(
+    prompt(
+      "Bienvenido a nuestro banco, ¿Que desea hacer? Presione 1 para DEPÓSITO - Presione 2 para EXTRACCIÓN - Presione 3 para VER SU SALDO - Presione 4 para SALIR"
+    )
   );
-};
 
-if (consulta === 1) {
-  let deposito = Number(prompt("Ingrese la cantidad que desea depositar"));
+  let depositar = (valorDepositar) => {
+    billetera += valorDepositar;
 
-  depositar(deposito);
-} else if (consulta === 2) {
-  console.log("Hola");
-} else {
-  console.log("Usted ingreso una opcion incorrecta, vuelva a comenzar.");
+    return alert(
+      `Usted ah depositado $${valorDepositar}, su saldo ahora es de $${billetera}. Muchas Gracias!! `
+    );
+  };
+
+  let extraccion = (valorExtraccion) => {
+    billetera -= valorExtraccion;
+
+    return alert(
+      `Su extracción se realizo correctamente, su saldo ahora es de $${billetera}`
+    );
+  };
+
+  if (consulta === 1) {
+    let deposito = Number(
+      prompt(
+        "Ingrese la cantidad que desea depositar, recuerde que el máximo es de $10.000"
+      )
+    );
+
+    if (deposito > 10000) {
+      alert(
+        `Usted no puede depositar $${deposito} ya que supera el límite diario, vuelva a intentarlo.`
+      );
+    } else {
+      depositar(deposito);
+    }
+  } else if (consulta === 2) {
+    let extraer = Number(prompt("Ingrese el dinero a extraer"));
+
+    if (billetera === 0 || billetera < extraer) {
+      alert(
+        `No puedes extraer $${extraer} ya que tu saldo es de $${billetera}`
+      );
+    } else {
+      extraccion(extraer);
+    }
+  } else if (consulta === 3) {
+    alert(`Su saldo es de $${billetera}`);
+  } else if (consulta === 4) {
+    alert(`Gracias por su visita, hasta pronto!!`);
+
+    value = false;
+  } else {
+    alert("Usted ingreso una opcion incorrecta, vuelva a comenzar.");
+  }
 }
+
